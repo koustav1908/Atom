bdcbb19be608bc658792b5d782c889ce3e006ea5
Koustav Sen <koustavsen17@gmail.com>
2021-02-14 02:22:03 +0530

    Added Sign Out

--- app/src/main/java/com/example/atom/ProfileActivity.java
+++ app/src/main/java/com/example/atom/ProfileActivity.java
@@ -1,18 +1,28 @@
 package com.example.atom;
 
+import android.app.LauncherActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity{
     Button logoutBtn;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     public void onStart() {
@@ -35,10 +45,22 @@ public class ProfileActivity extends AppCompatActivity{
                 startActivity(new Intent(ProfileActivity.this, MainActivity.class));
             }
         };
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mAuth.signOut();
+                mGoogleSignInClient.signOut()
+                        .addOnCompleteListener(ProfileActivity.this, new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                            }
+                        });
             }
         });
     }
